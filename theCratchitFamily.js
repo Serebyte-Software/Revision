305fe7c27fce1c3ec9ea5e8258879e7f86b7f648
Serebyte-Software <67656582+Serebyte-Software@users.noreply.github.com>
2020-12-25 21:47:50 +0000

    Add files via upload

--- theCratchitFamily.js
+++ theCratchitFamily.js
@@ -5,7 +5,7 @@ var quotes = [
 	["Mr Scrooge, the Founder of the Feast!","Bob Cratchit (Stave 3)"],
 	["odious, stingy, hard, unfeeling man as Mr Scrooge... I'll drink for your sake","Mrs Cratchit (Stave 3)"],
 	["Nobody said or thought it was a small pudding for a large family. It would have been flat heresy to do so","Narrator (Stave 3)"],
-	["He told me... it mgiht be pleasant to them to remember upon Christmas Day who made lame beggars walk, and blind men see","Bob Cratchit (Stave 3)"],
+	["He told me... it might be pleasant to them to remember upon Christmas Day who made lame beggars walk, and blind men see","Bob Cratchit (Stave 3)"],
 	["crammed spoons into their mouths, lest they should shriek for goose","Narrator (Stave 3)"],
 	["Mrs Cratchit made the gravy... Master Peter mashed the potatoes... Miss Belinda sweetened the apple sauce... Martha dusted the hot plates","Narrator (Stave 3)"],
 ];
